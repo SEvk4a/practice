de0c70f45a80bac198e14d0c71b7575d841d0336
Всеволод <sevk4a@192.168.100.5>
2025-08-14 13:58:11 +0300

    add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Test } from './components/test';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>123</div>
+    <Test />
   </StrictMode>,
-)
+);
